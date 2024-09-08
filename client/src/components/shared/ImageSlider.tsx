400ae19302ab825c7d956f33e24cd569f050af80
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-09-08 11:23:19 +0530

    Refactor ImageSlider component to dynamically load images and add navigation buttons

--- client/src/components/shared/ImageSlider.tsx
+++ client/src/components/shared/ImageSlider.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
-import SliderImage1 from "/sliderImage1.jpg";
-import SliderImage2 from "/sliderImage2.avif";
 
 const ImageSlider = () => {
   const [counter, setCounter] = useState<number>(0);
   const images = [
-    SliderImage1,
-    SliderImage2
+    "/sliderImage1.jpg",
+    "/sliderImage2.avif",
+    "/sliderImage3.avif",
+    "/imageSlider4.avif",
   ];
 
   useEffect(() => {
@@ -21,7 +21,7 @@ const ImageSlider = () => {
       <div className="relative w-full max-w-7xl h-[calc(100vh-230px)] overflow-hidden">
         {images.map((img, index) => (
           <div key={index} className="absolute inset-0 flex items-center justify-between">
-            <button className="z-10 p-2 bg-black bg-opacity-50 text-white rounded-full ml-4">
+            <button aria-label='icon1' disabled={counter == 0} onClick={() => counter !== 0 && setCounter((prev) => prev - 1)}  className="z-10 p-2 bg-black bg-opacity-50 text-white rounded-full ml-4">
               <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"></path>
               </svg>
@@ -33,7 +33,7 @@ const ImageSlider = () => {
               }`}
               alt={`Image Slider ${index + 1}`}
             />
-            <button className="z-10 p-2 bg-black bg-opacity-50 text-white rounded-full transform rotate-180 mr-4">
+            <button aria-label='icon2' disabled={counter == images.length-1} onClick={() => counter !== images.length-1 && setCounter((prev) => prev + 1)} className="z-10 p-2 bg-black bg-opacity-50 text-white rounded-full transform rotate-180 mr-4">
               <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"></path>
               </svg>
