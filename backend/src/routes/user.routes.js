a75de76115ac4d5233cb7829e9884138a75b84c1
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-09-15 11:49:13 +0530

    Refactor user routes and remove commented code

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,11 +2,11 @@ import { Router } from "express";
 import { changePassword, forgetPassword, getUserDetails, loginUser, logoutUser, registerUser, updateUserDetails } from '../controller/User.controller.js';
 import { verifyJWT } from "../middleware/auth.middleware.js";
 const userRouter = Router();
+userRouter.get('/',verifyJWT,getUserDetails)
 userRouter.post('/register',registerUser)
 userRouter.post('/login',loginUser)
 userRouter.post('/logout',logoutUser)
 userRouter.post('/update',verifyJWT,updateUserDetails)
-userRouter.get('/',verifyJWT,getUserDetails)
 userRouter.post('/forgetpassword',forgetPassword)
 // userRouter.post('/user/resetpassword',getUserDetails)
 userRouter.post('/changepassword',changePassword)
--- client/src/root/page/NewArrival.tsx
+++ client/src/root/page/NewArrival.tsx
@@ -15,7 +15,7 @@ const NewArrival = () => {
             <div className='flex flex-col items-center justify-center mt-10 border'>
         <h1 className='font-semibold text-2xl sm:text-3xl sm:mb-2'>New Arrival</h1>
         <p className='text-slate-400 text-sm sm:text-base mb-4 sm:mb-7 '>Upgrade your wardrobe with our must-have shirts</p>
-        {products &&  <ProductList products={products}/>}
+        {/* {products &&  <ProductList products={products}/>} */}
     </div>
         )
     }
