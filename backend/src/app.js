5e49eac653d119675be2a2dc9e1f07ef50d3ef85
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-10-03 15:04:47 +0530

    fix:vercel cant access public folder-2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import cors from 'cors'
-import userRouter from "./routes/user.routes.js"
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import userRouter from './routes/user.routes.js';
 import productRouter from './routes/product.routes.js';
 import { verifyJWT } from './middleware/auth.middleware.js';
 import sellerRouter from './routes/seller.routes.js';
@@ -12,20 +14,22 @@ import wishListRouter from './routes/wishlist.routes.js';
 import cartRouter from './routes/cart.routes.js';
 import orderRouter from './routes/Order.routes.js';
 import { promotionalRouter } from './routes/Promotional.routes.js';
-const app = express();
-app.use(express.json({ limit: '50mb' }));  
-app.use(express.urlencoded({ limit: '50mb', extended: true })); 
-app.use(express.static('./public'));
-app.get('/', (_, res) => {
-  res.send('Hello, world!');
-});
-app.use(cookieParser());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended : true}))
 
+const app = express();
+
+// Define __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware
+app.use(cookieParser());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+// CORS settings
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://cara-omega-six.vercel.app'
+  'https://cara-omega-six.vercel.app',
 ];
 
 const corsOptions = {
@@ -42,13 +46,21 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+// Static file serving
+app.use(express.static(path.join(__dirname, './public')));
+
+// Routes
+app.get('/', (_, res) => {
+  res.send('Hello, world!');
+});
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/productReview', verifyJWT, productReviewRouter);
-app.use('/api/v1/seller', verifyJWT,sellerRouter);
+app.use('/api/v1/seller', verifyJWT, sellerRouter);
 app.use('/api/v1/cart', verifyJWT, cartRouter);
 app.use('/api/v1/order', verifyJWT, orderRouter);
 app.use('/api/v1/wishlist', verifyJWT, wishListRouter);
 app.use('/api/v1/address', verifyJWT, addressRouter);
-app.use('/api/v1/promotional',verifyJWT,promotionalRouter)
+app.use('/api/v1/promotional', verifyJWT, promotionalRouter);
+
 export default app;
