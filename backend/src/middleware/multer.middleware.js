fd46115765edca81d899032c3b179bce4193b055
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-10-03 19:30:24 +0530

    fixing vercel error of files

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,13 +1,10 @@
-import multer from 'multer';
+import path from 'path';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public');
+    cb(null, path.join(__dirname, 'public')); // Using absolute path
   },
   filename: function (req, file, cb) {
-      cb(null, Date.now() + file.originalname)
+    cb(null, Date.now() + '-' + file.originalname); // Unique filename
   }
 });
-
-
-export const upload = multer({ storage: storage });
