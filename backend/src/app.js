8c3a024b1f44f440ac110a05c646e9f65ef1e92b
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-10-03 14:56:02 +0530

    fix:vercel cant access public folder-1

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ import { promotionalRouter } from './routes/Promotional.routes.js';
 const app = express();
 app.use(express.json({ limit: '50mb' }));  
 app.use(express.urlencoded({ limit: '50mb', extended: true })); 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 app.get('/', (_, res) => {
   res.send('Hello, world!');
 });
