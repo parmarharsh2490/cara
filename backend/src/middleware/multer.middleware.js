2881373aefdf9eaa4c75cd9baa841343525ba5ee
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-10-04 16:51:50 +0530

    fixing vercel folder problem

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,8 +1,14 @@
 import multer from 'multer';
 
+// const __filename = fileURLToPath(import.meta.url);
+// // Use the '/tmp' directory for temporary file storage
+// const uploadDir = '/tmp';
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir, { recursive: true });
+// }
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public');
+    cb(null, './../../tmp' );
   },
   filename: function (req, file, cb) {
       cb(null, Date.now() + file.originalname)
