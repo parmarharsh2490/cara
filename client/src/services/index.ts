8fe5d74092c7428be6ab110a58988769e6ab492a
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-10-11 12:50:22 +0530

    Update API endpoint in client services

--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL : import.meta.env.VITE_API_BASE_URL,
+  baseURL : import.meta.env.VITE_API_BASE_URL || "https://cara-backend-nine.vercel.app/api/v1",
   withCredentials: true
 });
 
