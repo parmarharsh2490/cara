7086ef8f5d795696c46f802e3859988f6fc4e72a
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-11-05 11:19:54 +0530

    uncomment import statement in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.js';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './context/index.js';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {
   QueryClient,
   QueryClientProvider,
