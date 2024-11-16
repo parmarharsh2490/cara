7361df1331640afaa4078642ce70873698c4fc3c
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-11-16 18:13:15 +0530

    Remove script tag from index.html and shift to ShoppingCart.ts to improve performance

--- client/src/utils/ShoppingCart.ts
+++ client/src/utils/ShoppingCart.ts
@@ -2,7 +2,6 @@ import { useRemoveFromCart, useUpdateQuantity } from "@/query/cart.queries";
 import { useCreateOrder, usePaymentHandler } from "@/query/orders.queries";
 import { useAddToWishlist } from "@/query/wishlist.queries";
 import { IUpdateCartQuantity } from "@/types";
-
 export const useShoppingCart = () => {
   const { mutateAsync: paymentHandler } = usePaymentHandler();
   const { mutateAsync: removeProductFromCart } = useRemoveFromCart();
@@ -11,21 +10,44 @@ export const useShoppingCart = () => {
   const { mutateAsync: createOrder, isPending: isOrderCreating } = useCreateOrder();
 
   const handlePayment = async (totalCartAmount: number) => {
+    const loadRazorpayScript = () => {
+      return new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.src = "https://checkout.razorpay.com/v1/checkout.js";
+        script.onload = () => resolve(true);
+        script.onerror = () => resolve(false);
+        document.body.appendChild(script);
+      });
+    };
+
+    const isScriptLoaded = await loadRazorpayScript();
+    if (!isScriptLoaded) {
+      console.error("Failed to load Razorpay script");
+      return;
+    }
+
     const orderId = await createOrder(totalCartAmount);
-    if (!orderId) return;
+    if (!orderId) {
+      console.error("Order creation failed");
+      return;
+    }
 
     const options = {
       key: import.meta.env.RAZORPAY_API_KEY,
       amount: totalCartAmount,
       currency: "INR",
       name: "Cara",
-      description: "Test Transaction dont worry just click it will not cut any money enter fake otp",
+      description: "Test Transaction - No actual payment.",
       order_id: orderId,
       handler: async (response: any) => {
-        await paymentHandler({ paymentResponse: response, totalCartAmount });
+        try {
+          await paymentHandler({ paymentResponse: response, totalCartAmount });
+        } catch (error) {
+          console.error("Payment processing failed", error);
+        }
       },
       prefill: {
-        name: "John Doe",
+        name: "John Doe", // Replace with dynamic user data
         email: "john.doe@example.com",
         contact: "9999999999",
       },
@@ -44,11 +66,9 @@ export const useShoppingCart = () => {
   }: IUpdateCartQuantity) => {
     if (quantity === 0) {
       await removeProductFromCart(cartProductId);
-      return;
+    } else {
+      await updateQuantity({ cartProductId, quantity });
     }
-
-    // Set the pending update which will trigger the debounced update
-    updateQuantity({ cartProductId, quantity });
   };
 
   const handleRemoveFromCart = async (cartProductId: string) => {
